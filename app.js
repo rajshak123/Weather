56855a32df50f3fb4f996b9527bbd5099640d169
rajshak123 <rickysterling9696@gmail.com>
2017-03-12 17:48:23 -0700

    YES1

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ geocode.geocode(address1,(errorMsg,results)=>{
     if(errorMsg)
         console.log(errorMsg);
     else {
-        console.log(JSON.stringify(results,undefined,2));
+        console.log(JSON.stringify(results.addressp, undefined, 2));
         forcst.getweather(results.lat,results.lng,(error,weatherResults)=>{
             if(error)
                 console.log("Error has occured");
-            else
-                console.log(JSON.stringify(weatherResults,undefined,2));
+            else {
+                console.log("Current Temperature in Fahrenheit: " + JSON.stringify(weatherResults.currentTemp, undefined, 2));
+                console.log("Current Relative humidity : " + JSON.stringify(weatherResults.humidity, undefined, 2));
+                console.log("Current wind speed in miles per hour : " + JSON.stringify(weatherResults.windSpeed, undefined, 2));
+            }
 
         });
 
--- forecast/forecastweather.js
+++ forecast/forecastweather.js
@@ -14,6 +14,7 @@ module.exports.getweather=(lat,lng,callback)=>{
         url:"https://api.darksky.net/forecast/96782d44ac8ae1a8a35d99a03c58658a/"+lat+","+lng,
 
             },(error,response,body)=>{
+            //console.log(JSON.parse(body));
                 if(error)
                     callback(error);
                 else if(response.statusCode===400)
@@ -21,8 +22,8 @@ module.exports.getweather=(lat,lng,callback)=>{
                 else {
                     callback(undefined,
                         {
-                        currentTemp:JSON.parse(body).currently.temperature,
-                        //apparentTemp:JSON.parse(body).currently.apparentTemperature,
+
+                            currentTemp: JSON.parse(body).currently.temperature,
                             humidity:JSON.parse(body).currently.humidity,
                             windSpeed:JSON.parse(body).currently.windSpeed,
                     });
